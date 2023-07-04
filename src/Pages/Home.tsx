df3fb36755cf9c983fa9088fc291ee06dbc560ac
shwnfhm <45961231+shwnfhm@users.noreply.github.com>
2023-07-04 17:32:47 -0500

    changed portfolio

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,28 @@
+import { Container, Typography, CssBaseline } from "@mui/material";
+import Futura from "../assets/fonts/Futura.ttf";
+
 const Home = () => {
-  return <h1>Coming Soon!</h1>;
+  return (
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{
+        backgroundColor: "#1f235c",
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CssBaseline />
+      <Typography
+        variant="h1"
+        sx={{ fontFamily: "Futura", textAlign: "center", color: "#fff" }}
+      >
+        Coming Soon!
+      </Typography>
+    </Container>
+  );
 };
 
 export default Home;
